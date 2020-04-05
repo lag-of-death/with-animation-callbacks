0b8d89dea77a7bd0a54a45e59eab6171c766af45
Mateusz Wit <mateuszwit21@gmail.com>
2020-04-05 15:54:17 +0200

    Create a POC for a HOC with callbacks for animating and transitioning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, {keyframes} from 'styled-components';
+
+const rotate = keyframes`
+  from {
+    width: 200px;
+    background: orange;
+  }
+
+  to {
+    width: 50px;
+  }
+`;
+
+const H1 = styled.h1`
+    animation: ${rotate} 2s linear;
+    
+    transition: all 2s;
+    
+    &:hover {
+      transform: translate(200px);
+    }
+`;
+
+function AnimationCallbacks(
+  {
+    animatedElement,
+    renderAnimatedElement,
+    onAnimationStarted,
+    onAnimationEnded,
+    onTransitionStarted,
+    onTransitionEnded
+  }) {
+
+  React.useEffect(() => {
+    animatedElement.current.addEventListener('animationstart', onAnimationStarted);
+    animatedElement.current.addEventListener('animationend', onAnimationEnded);
+
+    animatedElement.current.addEventListener('transitionstart', onTransitionStarted);
+    animatedElement.current.addEventListener('transitionend', onTransitionEnded);
+  }, [animatedElement, onAnimationEnded, onAnimationStarted, onTransitionEnded, onTransitionStarted]);
+
+  return renderAnimatedElement();
+}
 
 function App() {
+  const ref = React.useRef();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AnimationCallbacks
+        onTransitionStarted={() => {
+          console.log('onTransitionStarted');
+        }}
+        onTransitionEnded={() => {
+          console.log('onTransitionEnded');
+        }}
+        onAnimationStarted={() => {
+          console.log('onAnimationStarted')
+        }}
+        onAnimationEnded={() => {
+          console.log('onAnimationEnded')
+        }}
+        renderAnimatedElement={() => {
+          return (
+            <H1
+              ref={ref}>
+              abc
+            </H1>
+          );
+        }}
+        animatedElement={ref}
+      />
     </div>
   );
 }
